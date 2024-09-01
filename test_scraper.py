07e5c3f6f0c19468af02ec799971c386be947578
B.Lamine <medlamine.bellilet@outlook.com>
2024-09-01 08:32:05 -0700

    update data

--- test_scraper.py
+++ test_scraper.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup as bs
 import os
 from datetime import datetime
 import smtplib
+from email.message import EmailMessage
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
@@ -52,29 +53,30 @@ file_url = h3.parent.next_sibling.next_sibling.find('a').get('href')
 #print(file_url)
 filename = file_url.split('/')[-1]
 now = datetime.now()
-dirname = now.strftime('%Y-%m-%d' + '-pdf') # '/home/ubuntu/' +
-os.system('sudo mkdir {}'.format(dirname))
-filepath = dirname + '/' + filename
+dirname = 'pdf' + now.strftime('%Y%m%d%H%M%S') # '/home/ubuntu/' +
+os.system('sudo mkdir storage/{}'.format(dirname))
+filepath = 'storage/' + dirname + '/' + filename
 with open(filepath, 'wb') as f:
 	content = re.get(file_url, verify=False).content
 	f.write(content)
 
+#msg = EmailMessage()
 msg = MIMEMultipart()
 msg['To'] = ", ".join(mailing_list)
 msg['From'] = login[0]
 msg['Subject'] = "Test Report"
 
 body = MIMEText('Dear user; this is a test.', 'plain', 'utf-8')
+#body = 'Dear user; this is a test.'
 msg.attach(body)  # add message body (text or html)
+#msg.set_content(body)  # add message body (text or html)
 attachment = MIMEApplication(content, _subtype="xlsx")
 attachment.add_header('Content-Disposition','attachment', filename=filename)
 msg.attach(attachment)
+#msg.add_attachment(content, maintype="excel", subtype='xlsx')
 
 # Send the email
-print('host', login[2])
-print('user', login[0])
-print('pwd', login[1])
 with smtplib.SMTP(login[2], 25) as server:
     server.starttls()  # Secure the connection
     server.login(login[0], login[1])
-    server.sendmail(msg['From'], msg['To'], msg.as_string())
+    server.sendmail(msg['From'], mailing_list, msg.as_string())
